aa515db00c129ea7bc001b500c086a808f261766
jeMATHfischer <jefischer@uni-potsdam.de>
2019-03-20 14:16:17 +0100

    Improving on minor issues with the data output

--- TheHawkesPackage/ExponentialHawkes.py
+++ TheHawkesPackage/ExponentialHawkes.py
@@ -11,7 +11,7 @@ class ExponentialHawkes():
 
     def __init__(self, param):
         self.param = param
-        self.temporal = lambda x: self.param[0]*np.exp(-self.param[1]*x)
+        self.temporal = lambda x: self.param[1]*np.exp(-self.param[2]*x)
         self.Events = np.array([0])
         self.Sim_num = 0
 
@@ -20,15 +20,16 @@ class ExponentialHawkes():
         i = 0
 
         while i in range(k):
-            upper_bd = self.temporal(t - self.Events).sum()
+            upper_bd = self.param[0] + np.sum(np.array([self.temporal(self.Events[-1] - y) for y in self.Events]))
+            print(upper_bd)
 
             u = np.random.rand(1)
             tau = -np.log(u) / upper_bd
             t = t + tau
             s = np.random.rand(1)
 
-            if s <= self.temporal(t - self.Events).sum() / upper_bd:
-                self.Events = np.append(self.Events, T)
+            if s <= (self.param[0] + np.sum(np.array([self.temporal(t - y) for y in self.Events if y < t])))/ upper_bd:
+                self.Events = np.append(self.Events, t)
                 i += 1
 
         if self.Sim_num == 0:
@@ -38,6 +39,6 @@ class ExponentialHawkes():
 
     def intensity_over_interval(self, x):
         y = np.sort(np.append(x, self.Events))
-        return y, np.array([np.sum(np.array([self.temporal(k - j) for j in self.Events])) for k in y])
+        return y, np.array([np.sum(np.array([self.temporal(t - j) for j in self.Events if j < t])) for t in y])
 
 
